37ed2c55e38ef7b6577bf314d911b2f2f38391c4
Yasmin Ferreira <yasmincferreira8@gmail.com>
2023-03-09 15:38:33 -0400

    Create exercicio-10.c

---
+++ exercicio-10.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <conio.h>
+void main() 
+{
+    //Leia o sexo (‘h’ para homem e ‘m’ para mulher) e a altura de uma pessoa, e que calcula e mostra o seu peso ideal baseado na altura. Sabe-se que:
+        //a. Para homens: peso ideal = (72.7 * altura) - 58
+        //b. Para mulheres: peso ideal = (62.1 * altura) - 44.7
+
+}
